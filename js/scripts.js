259a78a7bee2a15a2cb0ff34706581b5f79b7d95
Catherine Bradley and David Mortkowitz <david@>
2018-08-15 09:50:13 -0700

    Add final backend logic, add final front-end interface.

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,25 @@
-$(document).ready(function(){
+
+$(document).ready(function(){ //frontend logic
   $("form#leap-year").submit(function(event){
   event.preventDefault();
   var year = parseInt($("input#year").val());
   var result = leapyear(year);
-  $("#result").text(result);
 
-  });
-var leapyear = function(year) {
-  if ((year % 4 === 0) && (year % 100 !== 0)) {
-    return true;
+  $(".year").text(year);
+
+  if (!result) {
+    $(".not").text("not");
   } else {
-    return false;
+    $(".not").text("");
   }
+  $("#result").show();
 
-
-};
-
-
-
+  }); //backend logic
+  var leapyear = function(year) {
+    if ((year % 4 === 0) && (year % 100 !== 0) || (year % 400 === 0)) {
+      return true;
+    } else {
+      return false;
+    }
+  };
 });
