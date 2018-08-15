054f3a973f0c026c183fd82f769081f17cd536cd
Catherine Bradley and David Mortkowitz <catherine@>
2018-08-15 09:35:49 -0700

    Add simply backend branching by refactoring.

--- js/scripts.js
+++ js/scripts.js
@@ -7,13 +7,13 @@ $(document).ready(function(){
 
   });
 var leapyear = function(year) {
-  if (year % 100 === 0) {
-    return false;
-  } else if (year % 4 === 0) {
+  if ((year % 4 === 0) && (year % 100 !== 0)) {
     return true;
   } else {
     return false;
   }
+
+
 };
 
 
